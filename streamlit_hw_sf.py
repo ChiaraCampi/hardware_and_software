d86c29399348f2c4000897c61733b59a7f6945c5
ChiaraCampi <94609553+ChiaraCampi@users.noreply.github.com>
2024-06-22 18:15:27 +0200

    Update streamlit_hw_sf.py

--- streamlit_hw_sf.py
+++ streamlit_hw_sf.py
@@ -1,1 +1,17 @@
+import numpy as np
+import 
+st.title("📊 Data evaluation app")
+
+st.write(
+    "We are so glad to see you here. ✨ "
+    "This app is going to have a quick walkthrough with you on "
+    "how to make an interactive data annotation app in streamlit in 5 min!"
+)
+
+st.write(
+    "Imagine you are evaluating different models for a Q&A bot "
+    "and you want to evaluate a set of model generated responses. "
+    "You have collected some user data. "
+    "Here is a sample question and response set."
+)
 
